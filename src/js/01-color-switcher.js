8d56986a2411ba1071816ef6c4b81506d343c9c7
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2021-12-17 14:10:40 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', changeBodyColor);
+refs.stopBtn.addEventListener('click', stopedChangeBodyColor);
+let timerId = null;
+
+function changeBodyColor() {
+    timerId = setInterval(() => { 
+        refs.body.style.backgroundColor = getRandomHexColor();
+        refs.startBtn.setAttribute('disabled', 'disabled');
+    }, 1000);
+    
+}
+
+function stopedChangeBodyColor() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
